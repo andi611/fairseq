2332b64bf8404b0b9e37cc4ced3059519246da38
andi611 <tingweiandyliu@gmail.com>
2023-03-20 21:24:52 +0800

    update for libri path

--- examples/wav2vec/wav2vec_manifest.py
+++ examples/wav2vec/wav2vec_manifest.py
@@ -21,47 +21,38 @@ def get_parser():
         "root", metavar="DIR", help="root directory containing flac files to index"
     )
     parser.add_argument(
-        "--valid-percent",
-        default=0.01,
-        type=float,
-        metavar="D",
-        help="percentage of data to use as validation set (between 0 and 1)",
-    )
-    parser.add_argument(
         "--dest", default=".", type=str, metavar="DIR", help="output directory"
     )
     parser.add_argument(
         "--ext", default="flac", type=str, metavar="EXT", help="extension to look for"
     )
-    parser.add_argument("--seed", default=42, type=int, metavar="N", help="random seed")
-    parser.add_argument(
-        "--path-must-contain",
-        default=None,
-        type=str,
-        metavar="FRAG",
-        help="if set, path must contain this substring for a file to be included in the manifest",
-    )
     return parser
 
+def is_train(file_path):
+    if "train-clean-100" in file_path:
+        return True
+    elif "train-clean-360" in file_path:
+        return True
+    elif "train-other-500" in file_path:
+        return True
+    else:
+        return False
+
+def is_valid(file_path):
+    if "dev-clean" in file_path:
+        return True
+    else:
+        return False
 
 def main(args):
-    assert args.valid_percent >= 0 and args.valid_percent <= 1.0
 
     if not os.path.exists(args.dest):
         os.makedirs(args.dest)
 
     dir_path = os.path.realpath(args.root)
-    if "LibriSpeech" in dir_path:
-        search_path = os.path.join(dir_path, "train-**/**/*." + args.ext)
-    else:
-        search_path = os.path.join(dir_path, "**/*." + args.ext)
-    rand = random.Random(args.seed)
+    search_path = os.path.join(dir_path, "**/*." + args.ext)
 
-    valid_f = (
-        open(os.path.join(args.dest, "valid.tsv"), "w")
-        if args.valid_percent > 0
-        else None
-    )
+    valid_f = open(os.path.join(args.dest, "valid.tsv"), "w")
 
     with open(os.path.join(args.dest, "train.tsv"), "w") as train_f:
         print(dir_path, file=train_f)
@@ -76,12 +67,16 @@ def main(args):
                 continue
 
             frames = soundfile.info(fname).frames
-            dest = train_f if rand.random() > args.valid_percent else valid_f
+            if is_train(file_path):
+                dest = train_f
+            elif is_valid(file_path):
+                dest = valid_f
+            else:
+                continue
             print(
                 "{}\t{}".format(os.path.relpath(file_path, dir_path), frames), file=dest
             )
-    if valid_f is not None:
-        valid_f.close()
+    valid_f.close()
 
 
 if __name__ == "__main__":
