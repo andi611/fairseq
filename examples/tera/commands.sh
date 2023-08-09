35783236176cfe4e9731a1d8abda9002df2aefef
andi611 <tingweiandyliu@gmail.com>
2023-08-09 16:11:22 +0800

    update useful training commands

---
+++ examples/tera/commands.sh
@@ -0,0 +1,3 @@
+# iter1
+# slim
+python3 fairseq_cli/hydra_train.py --config-dir /work/a129195789/fairseq/examples/tera/config/pretrain --config-name tera_small_librispeech task.data=/work/a129195789/fairseq/examples/hubert/simple_kmeans/data_dir/960 task.label_dir=/work/a129195789/fairseq/examples/hubert/simple_kmeans/label_dir/960
